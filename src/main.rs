ce20755ce255c8e27616ed70a9d4e29ae6fcc551
jerry-yu <anhuitianyu@163.com>
2024-06-12 13:15:52 +0800

    token address setting ok
    add claim op from client

--- src/main.rs
+++ src/main.rs
@@ -4,60 +4,128 @@ use solana_sdk::{
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
     signature::{Keypair, Signer},
-    transaction::Transaction,
+    transaction::Transaction,program_error::ProgramError
 };
 use std::str::FromStr;
-
-
-#[derive(BorshSerialize, BorshDeserialize, Debug, Default)]
-pub struct UserAccount {
-    pub credit: u32,
-    pub history: Vec<(i64, u8)>,
-}
-
-#[repr(C)]
-#[derive(BorshSerialize, BorshDeserialize, Debug, Default, Clone, PartialEq)]
-pub struct CreditSetting {
-    pub campain_id: u16,
-    pub level: u8,
-    pub daily_reward: u64,
-}
-
-#[repr(C)]
-#[derive(BorshSerialize, BorshDeserialize, Debug, Default, Clone, PartialEq)]
-pub struct CreditSettings {
-    pub settings: Vec<CreditSetting>,
-}
-
-#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
-pub enum CreditInstruction {
-    Init { settings: CreditSettings },
-    Add { pk: Pubkey, credit: u32 },
-    Update { pk: Pubkey, credit: u32 },
-}
+use sol_deeperchain::{ state::{UserAccount,TokenAccount,CreditSetting,CreditSettings},
+instruction::CreditInstruction,
+};
 
 fn main() {
     let client = RpcClient::new("http://127.0.0.1:8899".to_string());
-    
-    let sk = vec![99,145,152,156,220,165,60,157,4,136,243,62,204,38,190,94,82,201,134,153,100,230,34,243,31,21,168,253,209,59,204,192,205,65,234,255,47,147,140,84,234,176,144
-    ,169,157,219,170,161,56,150,167,230,253,232,139,25,60,121,62,74,230,112,206,32];
+
+    let sk = vec![
+        99, 145, 152, 156, 220, 165, 60, 157, 4, 136, 243, 62, 204, 38, 190, 94, 82, 201, 134, 153,
+        100, 230, 34, 243, 31, 21, 168, 253, 209, 59, 204, 192, 205, 65, 234, 255, 47, 147, 140,
+        84, 234, 176, 144, 169, 157, 219, 170, 161, 56, 150, 167, 230, 253, 232, 139, 25, 60, 121,
+        62, 74, 230, 112, 206, 32,
+    ];
     let payer = Keypair::from_bytes(&sk).unwrap();
 
     let program_id = Pubkey::from_str("HXM5ahXEvXRrZcGRfhQ5STqaopiGL1cMTYncEtUdxdTZ").unwrap();
 
-    let (init_pda, _bump_seed) = 
-        Pubkey::find_program_address(&[b"setting"], &program_id);
-    
+    // init credit setting config and token address
+    let (init_pda, _bump_seed) = Pubkey::find_program_address(&[b"credit_setting"], &program_id);
+    let (init_token, _bump_seed) = Pubkey::find_program_address(&[b"dpr_token"], &program_id);
+    let campaign_id:u16 = 1;
     let settings = CreditSettings {
-        settings: vec![CreditSetting{ campain_id:1,level:1,daily_reward:1},CreditSetting::default(),],
+        settings: vec![
+            CreditSetting {
+                campaign_id,
+                level: 1,
+                daily_reward: 1,
+            },
+            CreditSetting {
+                campaign_id,
+                level: 2,
+                daily_reward: 2,
+            },
+            CreditSetting {
+                campaign_id,
+                level: 3,
+                daily_reward: 3,
+            },
+        ],
     };
 
-    let init_instruction = CreditInstruction::Init { settings };
+    //DPR token address
+    let token = TokenAccount {
+        token: Pubkey::from_str("5kSfsEoPXv4cgKx4Ct2irz9xF6mWcTo1NLFfKfKs11fu").unwrap(),
+    };
+
+    let init_instruction = CreditInstruction::Init { settings, token };
 
     let instruction_data = to_vec(&init_instruction).unwrap();
 
     let accounts = vec![
         AccountMeta::new(payer.pubkey(), true),
+        AccountMeta::new(init_pda, false),
+        AccountMeta::new(init_token, false),
+        AccountMeta::new_readonly(solana_program::system_program::id(), false),
+    ];
+
+    let instruction = Instruction::new_with_bytes(program_id, &instruction_data, accounts);
+    let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+    let recent_blockhash = client.get_latest_blockhash().unwrap();
+    transaction.sign(&[&payer], recent_blockhash);
+    client.send_and_confirm_transaction(&transaction).unwrap();
+
+    //let dst_user = Pubkey::from_str("5kSfsEoPXv4cgKx4Ct2irz9xF6mWcTo1NLFfKfKs11fu").unwrap();
+
+    let dst_user = payer.pubkey();
+    // add credit record for users
+    let (user_credit, _bump_seed) =
+        Pubkey::find_program_address(&[b"user", dst_user.as_ref()], &program_id);
+    let credit_value: i32 = 199;
+
+    let add_instruction = CreditInstruction::Add {
+        pk: dst_user,
+        credit: credit_value,
+        campaign:campaign_id,
+        reward_since: 1,
+    };
+    let instruction_data = to_vec(&add_instruction).unwrap();
+
+    let accounts = vec![
+        AccountMeta::new(payer.pubkey(), true),
+        AccountMeta::new(user_credit, false),
+        AccountMeta::new_readonly(solana_program::system_program::id(), false),
+    ];
+
+    let instruction = Instruction::new_with_bytes(program_id, &instruction_data, accounts);
+    let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+    let recent_blockhash = client.get_latest_blockhash().unwrap();
+    transaction.sign(&[&payer], recent_blockhash);
+    client.send_and_confirm_transaction(&transaction).unwrap();
+
+    println!("Add PDA with credit value: {}", credit_value);
+
+    // set token address
+    let new_token_address = Pubkey::from_str("Fnz5v84PBHN3qLSz5heV3XucTnDy2TkUZD9EEC1Sr7Aw").unwrap();
+
+    let token_instruction = CreditInstruction::SetTokenAddress { address: new_token_address};
+    let instruction_data = to_vec(&token_instruction).unwrap();
+
+    let accounts = vec![
+        AccountMeta::new(payer.pubkey(), true),
+        AccountMeta::new(init_token, false),
+        AccountMeta::new_readonly(solana_program::system_program::id(), false),
+    ];
+
+    let instruction = Instruction::new_with_bytes(program_id, &instruction_data, accounts);
+    let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
+    let recent_blockhash = client.get_latest_blockhash().unwrap();
+    transaction.sign(&[&payer], recent_blockhash);
+    client.send_and_confirm_transaction(&transaction).unwrap();
+
+    //claim token
+    let claim_instruction = CreditInstruction::Claim;
+    let instruction_data = to_vec(&claim_instruction).unwrap();
+
+    let accounts = vec![
+        AccountMeta::new(payer.pubkey(), true),
+        AccountMeta::new(user_credit, false),
+        AccountMeta::new(init_token, false),
         AccountMeta::new(init_pda, false),
         AccountMeta::new_readonly(solana_program::system_program::id(), false),
     ];
@@ -68,55 +136,22 @@ fn main() {
     transaction.sign(&[&payer], recent_blockhash);
     client.send_and_confirm_transaction(&transaction).unwrap();
 
-
-
-    let user_account = Keypair::new();
-
-    // Initialize PDA with credit value
-    let (pda, _bump_seed) = 
-        Pubkey::find_program_address(&[b"user", user_account.pubkey().as_ref()], &program_id);
-    let credit_value: u32 = 100;
-    //let value: u32 = 200;
-    //let instruction_data = [0u8].iter().chain(user_account.pubkey().as_ref()).chain(&credit_value.to_le_bytes()).cloned().collect::<Vec<u8>>();
-
-    let add_instruction = CreditInstruction::Add { pk: user_account.pubkey(), credit: 100 };
-let instruction_data = to_vec(&add_instruction).unwrap();
-    
-
-    let accounts = vec![
-        AccountMeta::new(payer.pubkey(), true),
-        AccountMeta::new(pda, false),
-        AccountMeta::new_readonly(solana_program::system_program::id(), false),
-    ];
-
-    let instruction = Instruction::new_with_bytes(program_id, &instruction_data, accounts);
-    let mut transaction = Transaction::new_with_payer(&[instruction], Some(&payer.pubkey()));
-    let recent_blockhash = client.get_latest_blockhash().unwrap();
-    transaction.sign(&[&payer], recent_blockhash);
-    client.send_and_confirm_transaction(&transaction).unwrap();
-
-    println!("Add PDA with credit value: {}", 100);
-
-
-    //query
-    let data = client.get_account_data(&pda).unwrap();
+    //query credit value
+    let data = client.get_account_data(&user_credit).unwrap();
     let bytes: [u8; 4] = [data[3], data[2], data[1], data[0]];
 
-    // 将字节数组转换为 u32
     let num = u32::from_le_bytes(bytes);
-    let content = UserAccount::try_from_slice(&data[4..4+num as usize]).unwrap();
+    let content = UserAccount::try_from_slice(&data[4..4 + num as usize]).unwrap();
 
-    println!("Query PDA credit value: {:?}", content);
+    println!("Query PDA credit value: {:?} num {}", content,num);
 
     //query credit setting
     let data = client.get_account_data(&init_pda).unwrap();
-    // let bytes: [u8; 4] = [data[3], data[2], data[1], data[0]];
-
-    // // 将字节数组转换为 u32
-    // let num = u32::from_le_bytes(bytes);
     let content = CreditSettings::try_from_slice(&data).unwrap();
-
     println!("Query PDA credit settings: {:?}", content);
+    //query token address
+    let data = client.get_account_data(&init_token).unwrap();
+    let content = TokenAccount::try_from_slice(&data).unwrap();
 
-    
+    println!("Query PDA TokenAccount : {:?}", content);
 }
